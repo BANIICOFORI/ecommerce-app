dba0ef21645bcc7b9bdb8fde7e0fdcdd910d5675
baniicofori <baniicofori@gmail.com>
2023-07-01 18:59:34 +0000

    NEW_COMM

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter,Router,Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import OurStore from "./pages/OurStore"
+import Blog from './pages/Blog';
+import CompareProduct from './pages/CompareProduct';
+import Wishlist from './pages/Wishlist';
+import Login from './pages/Login';
+import Signup from './pages/Signup ';
+import Forgotpassword from './pages/Forgotpassword';
+import Resetpassword from './pages/Resetpassword';
+import SingleBlog from './pages/SingleBlog';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import RefundPloicy from './pages/RefundPloicy';
+import ShippingPolicy from './pages/ShippingPolicy';
+import TermAndContions from './pages/TermAndContions';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import SingleProduct from './pages/SingleProduct';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+  <BrowserRouter>
+  <Routes>
+<Route  path="/" element={<Layout />}>
+<Route index element={<Home />} />
+  <Route path="About" element={ <About />} />
+  <Route path="Contact" element={ <Contact />} />
+  <Route path="product" element={ <OurStore />} />
+  <Route path="product/:id" element={<SingleProduct />} />
+  <Route path="blogs" element={ <Blog />} />
+  <Route path="blog/:id" element={<SingleBlog />} />
+  <Route path="cart" element={<Cart />} />
+  <Route path="checkout" element={<Checkout />} />
+  <Route path="Compare-Product" element={<CompareProduct />} />
+  <Route path="wishlist" element={<Wishlist />} />
+  <Route path="login" element={<Login />} />
+  <Route path="signup" element={<Signup />} />
+  <Route path="forgot-password" element={<Forgotpassword />} />
+  <Route path="reset-password" element={<Resetpassword />} />
+  <Route path="privacy-policy" element={<PrivacyPolicy />} />
+  <Route path="refund-policy" element={<RefundPloicy />} />
+  <Route path="shipping-policy" element={<ShippingPolicy />} />
+  <Route path="term-conditions" element={<TermAndContions />} />
+</Route>
+  </Routes>
+  </BrowserRouter>
+  
+  </>
 }
 
 export default App;
---
+++ src/utils/Data.js
@@ -0,0 +1,27 @@
+const services = [
+    {
+    "title": "Free Shipping",
+    "tagline": "From all orders over 50",
+    "image": "images/service.png",
+},
+{
+    "title": "Free Shipping",
+    "tagline": "save upto 25% off",
+    "image": "images/service-02.png",
+},
+{
+    "title": "Free Shipping",
+    "tagline": "save upto 25% off",
+    "image": "images/service-03.png",
+},
+{
+    "title": "Free Shipping",
+    "tagline": "save upto 25% off",
+    "image": "images/service-03.png",
+},
+{
+    "title": "Free Shipping",
+    "tagline": "save upto 25% off",
+    "image": "images/service-03.png",
+},
+];
