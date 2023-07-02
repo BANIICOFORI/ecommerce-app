6096c76eeb8ca42f4f1e8961577dd35d79cd23df
baniicofori <baniicofori@gmail.com>
2023-07-02 13:14:57 +0000

    1111

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -218,8 +218,8 @@ const Home = () => {
           <div className="col-12">
             <h3 className="section-heading">Featured Collection</h3>
           </div>
-          <ProductCard />
-          <ProductCard />
+          {/* <ProductCard />
+          <ProductCard /> */}
           <ProductCard />
           <ProductCard />
         </div>
